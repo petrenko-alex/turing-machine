198752954270a6d0ee8bda85fd907f5bba5d8c43
Александр Петренко <gafk555@gmail.com>
2015-12-19 19:56:50 +0300

    Добавлены слоты всех кнопок.Соединены их сигналы.

--- TuringMachine/turingmachine.cpp
+++ TuringMachine/turingmachine.cpp
@@ -4,9 +4,56 @@ TuringMachine::TuringMachine(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	setConnections();
 }
 
 TuringMachine::~TuringMachine()
 {
 
+}
+
+void TuringMachine::importTape()
+{
+
+}
+
+void TuringMachine::exportTape()
+{
+
+}
+
+void TuringMachine::importController()
+{
+
+}
+
+void TuringMachine::exportController()
+{
+
+}
+
+void TuringMachine::machineStep()
+{
+
+}
+
+void TuringMachine::machineBeginWork()
+{
+
+}
+
+void TuringMachine::machineStopWork()
+{
+
+}
+
+void TuringMachine::setConnections()
+{
+	connect(ui.step, SIGNAL(clicked(bool)), SLOT(machineStep ()));
+	connect(ui.stop, SIGNAL(clicked(bool)), SLOT(machineStopWork()));
+	connect(ui.exportTape, SIGNAL(clicked(bool)), SLOT(exportTape()));
+	connect(ui.importTape, SIGNAL(clicked(bool)), SLOT(importTape()));
+	connect(ui.begin, SIGNAL(clicked(bool)), SLOT(machineBeginWork()));
+	connect(ui.exportController, SIGNAL(clicked(bool)), SLOT(exportController()));
+	connect(ui.importController, SIGNAL(clicked(bool)), SLOT(importController()));
 }
--- TuringMachine/turingmachine.h
+++ TuringMachine/turingmachine.h
@@ -2,7 +2,12 @@
 #define TURINGMACHINE_H
 
 #include <QtWidgets/QMainWindow>
+#include <QFileDialog>
+#include <QDebug>
 #include "ui_turingmachine.h"
+
+#define DEFAULT_CONTROLLER_FILE "./controller.json"
+#define DEFAULT_TAPE_FILE		"./tape.json"
 
 class TuringMachine : public QMainWindow
 {
@@ -12,7 +17,19 @@ public:
 	TuringMachine(QWidget *parent = 0);
 	~TuringMachine();
 
+private slots:
+	void importTape();
+	void exportTape();
+	void importController();
+	void exportController();
+	void machineStep();
+	void machineBeginWork();
+	void machineStopWork();
+
 private:
+	void setConnections();
+
+
 	Ui::TuringMachineClass ui;
 };
 
